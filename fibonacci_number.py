a191eef2a118f8ac4eeb5718e1b9cb917c0816be
Gulay <gulay.muradli@icloud.com>
2026-08-22 22:00:20 +0400

    Add files via upload

---
+++ fibonacci_number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n):
+        if n <= 1:
+            return n
+
+        a = 0
+        b = 1
+
+        for i in range(2, n + 1):
+            a, b = b, a + b
+
+        return b
